ef862ab4c22ff8f260ead99bdf9a07b94dfdf4dd
fighterrbd <fighterrbd@gmail.com>
2025-05-24 00:42:49 +0530

    Update play.py

--- EsproMusic/utils/inline/play.py
+++ EsproMusic/utils/inline/play.py
@@ -60,12 +60,15 @@ def stream_markup_timer(_, chat_id, played, dur):
           #  InlineKeyboardButton(text="➘", callback_data=f"ADMIN Stop|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
-        [
-           # InlineKeyboardButton(
-                #text=f"{played} {bar} {dur}",
-                #callback_data="GetTimer",
-        InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)   
-            
+        
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+                ], 
+                
+        [InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",),   
+
+                ]     
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
